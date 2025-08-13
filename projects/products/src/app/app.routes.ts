40a542677b2d5566aa4a7ad9b2b3f35eb2ab511a
tukur-eng <fatiuerp@gmail.com>
2025-08-13 23:24:53 +0100

    layout

--- projects/products/src/app/app.routes.ts
+++ projects/products/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: 'product-dash', component: ProductDashboardComponent },
+    { path: '', redirectTo: '/Home', pathMatch: 'full' },
+   
+    { path: 'Home', component: AppComponent },
+
+
+  
+];
---
+++ projects/products/src/app/product-dashboard/product-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-product-dashboard',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './product-dashboard.component.html',
+  styleUrl: './product-dashboard.component.css'
+})
+export class ProductDashboardComponent {
+
+}
