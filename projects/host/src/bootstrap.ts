de39e413dff2ebc11afa7ff35ab1a7af3b5b44dd
ismailoladimeji <ismailadekambi@gmail.com>
2025-08-14 13:03:40 +0100

    comm

--- projects/host/src/bootstrap.ts
+++ projects/host/src/bootstrap.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { appConfig } from 'projects/products/src/app/app.config';
+// import { appConfig } from 'projects/products/src/app/app.config';
 import { ProductDashboardComponent } from 'projects/products/src/app/product-dashboard/product-dashboard.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 //import { AppComponent } from './app/app.component';
-bootstrapApplication(ProductDashboardComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(ProductDashboardComponent, appConfig)
+//   .catch((err) => console.error(err));
